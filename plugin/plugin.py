46bda3d172aa55b3f8cd8fdc83efdfe8208e6c91
meo <lupomeo@gmail.com>
2011-10-04 16:21:21 +0200

    populate tree

---
+++ plugin/plugin.py
@@ -0,0 +1,151 @@
+from Screens.Screen import Screen
+from Screens.MessageBox import MessageBox
+from Screens.VirtualKeyBoard import VirtualKeyBoard
+
+from Components.ActionMap import ActionMap
+from Components.config import config, ConfigText, ConfigSubsection, getConfigListEntry, NoSave
+from Components.ConfigList import ConfigListScreen
+from Components.ActionMap import ActionMap
+from Components.Label import Label
+from Components.Pixmap import Pixmap
+from Components.Sources.List import List
+from Plugins.Plugin import PluginDescriptor
+from random import Random 
+from telnetlib import Telnet
+import string
+
+
+class SetPasswdMain(Screen, ConfigListScreen):
+	skin = """
+		<screen position="center,center" size="585,250" title="Set Root Password" >
+		<widget name="config" position="10,10" size="565,200" scrollbarMode="showOnDemand" />
+		<ePixmap pixmap="skin_default/buttons/red.png" position="5,210" size="140,40" alphatest="on" />
+		<ePixmap pixmap="skin_default/buttons/green.png" position="150,210" size="140,40" alphatest="on" />
+		<ePixmap pixmap="skin_default/buttons/yellow.png" position="295,210" size="140,40" alphatest="on" />
+		<ePixmap pixmap="skin_default/buttons/blue.png" position="440,210" size="140,40" alphatest="on" />
+		<widget name="key_red" position="5,210" zPosition="1" size="140,40" font="Regular;17" halign="center" valign="center" backgroundColor="#9f1313" transparent="1" />
+		<widget name="key_green" position="150,210" zPosition="1" size="140,40" font="Regular;17" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+		<widget name="key_yellow" position="295,210" zPosition="1" size="140,40" font="Regular;17" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+		<widget name="key_blue" position="440,210" zPosition="1" size="140,40" font="Regular;17" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+	</screen>"""
+
+	def __init__(self, session):
+		Screen.__init__(self, session)
+		
+		self.skin = SetPasswdMain.skin
+		self.list = []
+		ConfigListScreen.__init__(self, self.list)
+		
+		self["key_red"] = Label(_("Set Password"))
+		self["key_green"] = Label(_("Generate"))
+		self["key_yellow"] = Label(_("Virtual Keyb"))
+		self["key_blue"] = Label(_("Cancel"))
+
+		self["actions"] = ActionMap(["OkCancelActions", "ColorActions"],
+		{
+			"red": self.DoConnectPass,
+			"green": self.greenPressed,
+			"yellow": self.yellowPressed,
+			"blue": self.close,
+			"cancel": self.close
+		}, -1)
+	
+		self.newpass = self.buildPass()
+		self.oldp = NoSave(ConfigText(fixed_size = False, default = ""))
+		self.newp = NoSave(ConfigText(fixed_size = False, default = self.newpass))
+		self.updateList()
+	
+	def updateList(self):
+		self.list = []
+		self.list.append(getConfigListEntry('Enter old Password', self.oldp))
+		self.list.append(getConfigListEntry('Enter new Password', self.newp))
+		self["config"].list = self.list
+		self["config"].l.setList(self.list)
+		
+	def buildPass(self):
+		passwd = string.letters + string.digits
+		return ''.join(Random().sample(passwd, 8)) 
+
+	def greenPressed(self):
+		self.newp.value = self.buildPass()
+		self.updateList()
+		
+	def yellowPressed(self):
+		sel = self["config"].getCurrent()
+		value = self.oldp.value
+		self.valuetype = 0
+		if sel[0] == "Enter new Password":
+			value = self.newp.value
+			self.valuetype = 1	
+		self.session.openWithCallback(self.virtualKeybDone, VirtualKeyBoard, title=sel[0], text=value)
+		
+	
+	def virtualKeybDone(self, passw):
+		if self.valuetype == 0:
+			self.oldp.value = passw
+		else:
+			self.newp.value = passw
+		self.updateList()
+
+	def DoConnectPass(self):
+		self.session.open(SetPasswdDo, self.oldp.value, self.newp.value)
+
+
+class SetPasswdDo(Screen):
+	skin = """
+		<screen position="center,center" size="520,400" title="Set Root Password" >
+			<widget name="lab" position="0,0" size="520,400" font="Regular;20" />
+		</screen>"""
+
+	def __init__(self, session, oldp, newp):
+		Screen.__init__(self, session)
+				
+		self["lab"] = Label("")
+		self["actions"] = ActionMap(["WizardActions"],
+			{
+				"ok": self.end,
+				"back": self.end,
+			}, -1)
+		
+		self.oldp = oldp
+		self.newp = newp
+		self.connected = True
+		self.connect()
+		
+	def connect(self):
+		tn = Telnet("localhost")
+		out = tn.read_until("login:", 3)
+		tn.write("root\n")
+		check = tn.read_until("Password:", 2)
+		out += check
+		if check.__contains__("Password:"):
+			tn.write(self.oldp + "\n")
+			check = tn.read_until("~#", 2)
+			out += check
+		if check.__contains__("~#"):
+			tn.write("passwd\n")
+			out += tn.read_until("password", 2)
+			tn.write(self.newp + "\n")
+			out += tn.read_until("password", 2)
+			tn.write(self.newp + "\n")
+			out += tn.read_until("xxx", 1)
+			tn.write("exit\n")
+			out += tn.read_all()
+		else:
+			out += "\nLogin incorrect, wrong password."
+			tn.close()
+		
+		self.connected = False
+		self["lab"].setText(out)
+	
+	def end(self):
+		if self.connected == False:
+			self.close()
+
+		
+def main(session, **kwargs):
+	session.open(SetPasswdMain)
+
+def Plugins(**kwargs):
+	return PluginDescriptor(name="Change root password", description=_("Change the root password of your box"), where = PluginDescriptor.WHERE_PLUGINMENU, fnc=main)
+	
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup, Extension
+
+pkg = 'Extensions.ChangeRootPassword'
+setup (name = 'enigma2-plugin-extensions-changerootpassword',
+       version = '0.1',
+       license='GPLv2',
+       url='https://github.com/E2OpenPlugins',
+       description='ChangeRootPassword',
+       long_description='Set/Change your box password',
+       author='meo',
+       author_email='lupomeo@hotmail.com',
+       packages = [pkg],
+       package_dir = {pkg: 'plugin'}
+      )
